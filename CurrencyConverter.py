8e3da1446141d54156e8644ddf6f6c275a58d003
Ayoub EAbidawi <ayowb12@gmail.com>
2024-11-11 14:43:57 -0500

    creating project

---
+++ CurrencyConverter.py
@@ -0,0 +1,13 @@
+#Assignment 4
+
+#Creat a menue
+
+print("What would you like to do today ?")
+menu_option = int(input("1. list the available currencies\n2. Convert an amount from a base currency to another\nOption number:  "))
+
+if menu_option == 1 :
+    pass
+elif menu_option == 2:
+    pass
+else:
+    print("You have entered Unvalid option")
